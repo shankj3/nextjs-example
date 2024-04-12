d5662d5af2b0462f2698c3458735f3ecadf27829
Jessi Shank <jessi.shank@rollbar.com>
2024-04-12 15:45:56 -0700

    make prettier buton

--- app/testerror.tsx
+++ app/testerror.tsx
@@ -1,6 +1,45 @@
 'use client'
-import { useRollbar } from '@rollbar/react'
+import { ErrorBoundary, Provider, useRollbar } from '@rollbar/react'
 import { useEffect } from 'react'
+
+const rollbarConfig = {
+    accessToken: process.env.NEXT_PUBLIC_ROLLBAR_CLIENT_TOKEN,
+    captureUncaught: true,
+    captureUnhandledRejections: true,
+    environment: 'production',
+    server: {
+      root: "https://nextjs-example-beta-ten.vercel.app/",
+    },
+    code_version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+    payload: {
+      environment: 'production',
+      client: {
+        javascript: {
+          source_map_enabled: true, // false by default
+          
+          // -- Add this into your configuration ---
+          code_version: process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+          // ---------------------------------------
+          
+          // Optionally have Rollbar guess which frames the error was 
+          // thrown from when the browser does not provide line 
+          // and column numbers.
+          guess_uncaught_frames: true
+        }
+      }
+    }
+  };
+  
+
+export function TestRollbarWrappedComponent() {
+    return (
+    <Provider config={rollbarConfig}>
+        <ErrorBoundary>
+          <TestError/>
+        </ErrorBoundary>
+      </Provider>
+    )
+}
 
 export function TestError() {
   const rollbar = useRollbar()
@@ -13,7 +52,9 @@ export function TestError() {
   return (
     <div>
     meomeow I throw an error 
-    <button onClick={onClick}>throw an exception</button>
+    <button 
+        className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded" 
+        onClick={onClick}>throw an exception</button>
     </div>
   );
 }
