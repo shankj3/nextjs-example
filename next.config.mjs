adf381b571dd192159d9a08bb03ec3e96f1186fe
Jessi Shank <jessi.shank@rollbar.com>
2024-04-12 15:58:04 -0700

    attempt to use hidden-source-map

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,18 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     productionBrowserSourceMaps: true,
-//     webpack: (config) => {
-//         config.devtool = 'nosources-source-map'
-//     }
-// }
 import RollbarSourcemapPlugin from "rollbar-sourcemap-webpack-plugin"
-// const RollbarSourcemapPlugin = require('rollbar-sourcemap-webpack-plugin');
-// import withSourceMaps from "@zeit/next-source-maps"
 
-// replace `<ROLLBAR_ACCESS_TOKEN>` with your Rollbar access token
+// this is an environment variable I added to my vercel - it isn't prefixed 
+// with NEXT_PUBLIC, which is good because we only need it for the build process 
+// cuz this ist he public token 
 const ROLLBAR_ACCESS_TOKEN = process.env.ROLLBAR_SERVER_TOKEN;
 
 const nextConfig = {
     productionBrowserSourceMaps: true,
     webpack: (config, { dev, webpack, buildId }) => {
         if (!dev) {
-            /* eslint-disable-next-line no-param-reassign */
-            // config.output.futureEmitAssets = false;
-            config.devtool = 'nosources-source-map'
+            // THIS IS IMPORTANT 
+            config.devtool = 'hidden-source-map'
+            // this is already exposed by vercel
             const codeVersion = process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA;
             console.log(ROLLBAR_ACCESS_TOKEN, codeVersion)
             config.plugins.push(
